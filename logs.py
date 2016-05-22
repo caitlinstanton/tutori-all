e69be5809c3a56f99673f87e988b7c7251fd59ab
Jion Fairchild <jijiglobe@yahoo.com>
2016-05-21 22:43:41 -0400

    begin email verification code

--- logs.py
+++ logs.py
@@ -5,32 +5,50 @@ import smtplib
 from email.mime.text import MIMEText
 
 
-
+#creates a log in the users log file with a message and a timestamp
+#returns True upon successful completion
 def log(user, message):
     try:
+        #open file at logs/user"
         logFile = open("logs/"+user+".txt","a")
+
+        #generate timestamp
         currentTime = time.strftime("%c")
-        #print "time generated"
+
+        #write message to file
         logFile.write(currentTime+": "+message+"\n")
         logFile.close()
         return True
     except:
+        #you're actuall pretty screwed... you can't exactly read the log file if the log function is broken...
         return False
 
+#sends an email from the addres: stuyaristabot@gmail.com to user
+#returns True upon successful completion, otherwise returns false and makes a log in the user's logs file
 def email(user, subject, message):
-    s = smtplib.SMTP('smtp.gmail.com',587)
-    s.ehlo()
-    s.starttls()
-    
-    s.login("stuyaristabot@gmail.com","stuy arista")
+    try:
+        #open connection to gmail login sever
+        #ports may need to be updated if google updates their smtp server
+        s = smtplib.SMTP('smtp.gmail.com',587)
+        
+        #sets mode to extended smtp
+        s.ehlo()
 
-
-    message = MIMEText("message")
-    message["Subject"] = subject
-    sender = message["From"] = "stuyaristabot@gmail.com"
-    recipient = message["To"] = "jijiglobe@yahoo.com"
-    s.sendmail(sender, [recipient], message.as_string())
-    
-    s.quit()
-
-email("jijiglobe@yahoo.com", "test", "more test")
+        #start connection encryption
+        s.starttls()
+        
+        #login to stuyaristabot@gmail.com
+        s.login("stuyaristabot@gmail.com","stuy arista")
+        
+        
+        message = MIMEText(message)
+        message["Subject"] = subject
+        sender = message["From"] = "stuyaristabot@gmail.com"
+        recipient = message["To"] = "jijiglobe@yahoo.com"
+        s.sendmail(sender, [recipient], message.as_string())
+        
+        s.quit()
+        return True
+    except:
+        log(user,"unable to send message: "+message)
+        return False
--- security.py
+++ security.py
@@ -1,65 +1,72 @@
-from logs import log
+from logs import *
 from passlib.hash import pbkdf2_sha256
 import pymongo
 from pymongo import MongoClient
+import random, string
+from globalVars  import *
+
+#generates a random lowercase String
+def generateRandomString(length):
+   return ''.join(random.choice(string.lowercase+string.ascii_uppercase+string.digits) for i in range(length))
 
 #Runs full account creation script and returns message indicating operation status
 def createAccount(username, password, counselor, homeroom, firstName, lastName):
-    if checkUsername(username,password):
-        log(username, "attempted account creation")
-        return "Email in use"
-    else:
-        if addUser(username, password, counselor, homeroom, firstName, lastName):
-            log(username, "account created")
-            return "Account creation successful"
-        else:
-            log("sys","an unknown error accured during account creation")
-            return "Account creation failed"
+   if checkUsername(username,password):
+      log(username, "attempted account creation")
+      return "Email in use"
+   else:
+      code = generateRandomString(20)
+      if addUser(username, password, counselor, homeroom, firstName, lastName, code):
+         log(username, "account created")
+         return "Account creation successful"
+      else:
+         log("sys","an unknown error accured during account creation")
+         return "Account creation failed"
 
 #Adds a user to database
-def addUser(username, password, counselor, homeroom, firstName, LastName):
-    try:
-        user = {"username": username, "hash": hashPass(password) "credits": {}, "isTutor": True,"classes": {},
-                "guidanceCounselor": counselor,"homeRoom":homeroom,
-                "frees":[],"goodClasses":[],
-                "firstName": firstName, "lastName":lastName}
-        db.users.insert(user)
-        return True
-    except:
-        return False
+def addUser(username, password, counselor, homeroom, firstName, LastName, emailVerificationCode):
+   try:
+      user = {"username": username, "hash": hashPass(password), "credits": {}, "isTutor": True,"classes": {},
+              "guidanceCounselor": counselor,"homeRoom":homeroom,
+              "frees":[],"goodClasses":[],
+              "firstName": firstName, "lastName":lastName}
+      db.users.insert(user)
+      return True
+   except:
+      return False
 
 #Returns boolean that indicates whether a user has inputted correct login information
 def authenticate(username, password):
-    try:
-        user = db.users.find({"name": username})
-        check = verify(password,user[0]["hash"])
-        if check:
-            log(username,"logged in")
-        else:
-            log(username,"failed login was attempted")
-    except:
-        log(username, "an unknown user authentication error occurred")
-        return False
-
+   try:
+      user = db.users.find({"name": username})
+      check = verify(password,user[0]["hash"])
+      if check:
+         log(username,"logged in")
+      else:
+         log(username,"failed login was attempted")
+   except:
+      log(username, "an unknown user authentication error occurred")
+      return False
+   
 #Returns boolean indicating whether a certain username is taken
 def checkUsername(username):
-    try:
-        user = db.users.find({"name": username})
-        if len(users >=1):
-            return True
-        else:
-            return False
-    except:
-        return False
+   try:
+      user = db.users.find({"name": username})
+      if len(users >=1):
+         return True
+      else:
+         return False
+   except:
+      return False
 
 #Removes user from the database
 def deleteUser(username):
-    pass
+   pass
 
 # Hashes and returns password that is hashed and salted by 29000 rounds of pbkdf2 encryption                                          
 def hashPass(password):
-    return pbkdf2_sha256.encrypt(password)
+   return pbkdf2_sha256.encrypt(password)
 
 # Returns true if password hashes into hashpass, false otherwise                                                                      
 def verify(password, hashpass):
-    return pbkdf2_sha256.verify(password,hashpass)
+   return pbkdf2_sha256.verify(password,hashpass)
