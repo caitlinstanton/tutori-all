612eb4e2c8b883689f644851fbc808b918f6a96e
Yasmeen Roumie <y4smeen@gmail.com>
2016-05-22 02:01:22 -0400

    add dashboard template

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 #For this code to work you need to install flask using either "pip install flask" or "sudo apt-get install python-flask"
-#then you will be able to import the following things into your code 
+#then you will be able to import the following things into your code
 #they are a bunch of functions that you will learn how to use, though not all today
 from flask import Flask, render_template, session, request, redirect, url_for
 
@@ -15,15 +15,15 @@ def index():
     #This function is called index() because usually the defualt page of a website is referred to as the index
 
     #for the purposes of this demo we're going to simply redirect to /page1
-    return redirect('/page1')
+    return redirect('/home')
 
 
-@app.route('/page1')
+@app.route('/home')
 def page1():
     #This is Page1, we want to return the html of page1 as a website so it can be served to the client
     #The default location for html in flask is /templates so if you put an html file in the /templates directory
     #You can access it easily
-    #This code returns the website stored in main.html 
+    #This code returns the website stored in main.html
     return render_template('base.html')
 
 
@@ -40,7 +40,7 @@ if __name__ == '__main__':
     #normally you would store it offline so people can't just see it
     #but this is a demo
     app.secret_key = 'DONT PUT THIS ON GITHUB IF YOU WANT SECURITY'
-    
+
     #finally this line runs the code
     #The first parameter is the place you want to run it
     #  0.0.0.0 is localhost: your current machine
