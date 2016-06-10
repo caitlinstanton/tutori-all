0e9e44af25f5a02a4f6b03506fe8079ade711ce8
Jion Fairchild <jion.fairchild@gmail.com>
2016-06-09 21:19:45 -0400

    added documentation to the crediting file

--- crediting.py
+++ crediting.py
@@ -4,13 +4,16 @@ import datetime
 
 twoWeeks = 15 * 60 * 60 * 24
 
+#initializes first two trimesters
 def initializeTrimesters(trimester1,trimester2):
     db.globals.insert({"name":"trimesters", 
                        "value":[getUnixTime(trimester1),getUnixTime(trimester2)] })
 
+#gets a list in unix time of start dates for trimesters
 def getTrimesterList():
     return db.globals.find({"name":"trimesters"})[0]["value"]
 
+#adds a trimester to the trimester list
 def addTrimester(date):
     globalData = db.globals.find({"name":"trimesters"})[0]
     trimesterList = getTrimesterList()
@@ -18,6 +21,7 @@ def addTrimester(date):
     globalData["value"] = trimesterList
     db.globals.update({"name":"trimesterList"}, globalData)
     
+#changes a trimester start date to a different date (UNTESTED)
 def changeTrimester(oldDate,newDate):
     oldTime = getUnixTime(oldDate)
     newTime = getUnixTime(newDate)
@@ -33,12 +37,15 @@ def changeTrimester(oldDate,newDate):
             db.users.update({"username":user["username"]},user)
     db.globals.update({"name":"trimesters"},current)
 
+#takes a string in the mm-dd-yyyy format and converts it to unix time
 def getUnixTime(date):
     return int(time.mktime(datetime.datetime.strptime(date, "%m-%d-%Y").timetuple()))
 
+#takes a unix time and converts it to a string in the mm-dd-yyyy format
 def getDate(unixTime):
     return datetime.datetime.fromtimestamp(int(unixTime)).strftime('%m-%d-%Y')
 
+#adds credits to a user dict given the date and the number of credits for the session
 def addCredits(tutor,date,numCredits):
     taimu = getUnixTime(date)
     current = int(time.time())
