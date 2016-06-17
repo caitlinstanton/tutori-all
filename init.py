518cab729c2078103937e45d1f9861986631a7c4
Jion Fairchild <jion.fairchild@gmail.com>
2016-06-17 09:17:05 -0400

    finish init.py

--- init.py
+++ init.py
@@ -1,8 +1,16 @@
-import pymongo
-from pymongo import MongoClient
+from sessions import *
+from crediting import *
+db.users.remove({})
+db.sessions.remove({})
 
-connection = MongoClient()
-db = connection.database
-
-users = db["users"]
-sessions = db["sessions"]
+print "please select and admin password:"
+password = raw_input()
+createAccount("admin@gmail.com",password,"n/a","n/a","admin","admin")
+changeValue("admin@gmail.com","isAdmin",True)
+changeValue("admin@gmail.com","isVerified",True)
+print "you can use this password with the email 'admin@gmail.com' to log in"
+print "choose the start date of the last trimester (mm-dd-yyyy):"
+last = raw_input()
+print "choose the start date of the current trimester (mm-dd-yyyy):"
+current = raw_input()
+initializeTrimesters(last,current)
